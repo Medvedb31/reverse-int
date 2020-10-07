cfe4bc9e28fa549cb2ed08d9c656bfea0dfcb4ac
Dmitry Cheplyanski <cheplyanskii2011@mail.ru>
2020-10-07 16:33:33 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        return parseInt(String(n).slice(1).split('').reverse().join(''));
+    }
+    else return parseInt(String(n).split('').reverse().join(''));
 }
